e5086c392c9d56a194901bee26e542b0c151abc6
vagrant <vagrant@precise32.(none)>
2014-01-22 23:25:17 +0000

    added javascript shenanigans

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,37 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("hey");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(formClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       $(description).fadeOut();
+    }
+}
+
+function formClick(e) {
+	e.preventDefault();
+	console.log("hey");
+	var projectToChange = $("#project").val();
+	var projectDescription = $(projectToChange).find(".project-description");
+	$(projectDescription).text($("#description").val());
+	$(projectToChange).find("#myImg").animate({
+		width: 500px;
+	}, 1000);
 }
